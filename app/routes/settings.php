77e665dd31a7901bfd1064856936d3b7d1b71d24
Yasser Mohamed Albarbary <yasser.al-barbary@live.com>
2018-08-19 16:49:43 +0200

    cc

--- app/routes/settings.php
+++ app/routes/settings.php
@@ -17,16 +17,16 @@ return [
         ],
         //Details needed for Merchant creation. Use ReadMe for details on how to set these variables.
         'configArray' => [
-            'gatewayBaseUrl' => getenv('https://migs.mastercard.com.au/vpcpay'),
+            'gatewayBaseUrl' => getenv('GATEWAY_BASE_URL'),
             'pkiBaseUrl' => getenv('PKI_BASE_URL'),
-            'hostedSessionUrl' => getenv('https://migs.mastercard.com.au/vpcpay') . '/form',
-            'gatewayUrl' => getenv('https://migs.mastercard.com.au/vpcpay') . '/api/rest',
-            'merchantId' => getenv('CIB700500'),
-            'apiUsername' => "merchant." . getenv('CIB700500'),
+            'hostedSessionUrl' => getenv('GATEWAY_BASE_URL') . '/form',
+            'gatewayUrl' => getenv('GATEWAY_BASE_URL') . '/api/rest',
+            'merchantId' => getenv('GATEWAY_MERCHANT_ID'),
+            'apiUsername' => "merchant." . getenv('GATEWAY_MERCHANT_ID'),
             'password' => getenv('GATEWAY_API_PASSWORD'),
             'debug' => 'FALSE',
             'version' => getenv('GATEWAY_API_VERSION') ?: '45' ,
-            'currency' => getenv('GATEWAY_DEFAULT_CURRENCY') ?: 'EGP',
+            'currency' => getenv('GATEWAY_DEFAULT_CURRENCY') ?: 'USD',
             'certificatePath' => getenv('GATEWAY_SSL_CERT_PATH'),
             //IMPORTANT: Ensure that you set these flags to TRUE in Production. The Test certificate is self signed and doesn't really need these to be set in Development.
             // By default they are set to PRODUCTION env values
